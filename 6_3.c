528c94ecd6383078b1f94cdde0ea97f720642cdd
sullihuk <nyxa@nextmail.ru>
2023-05-28 23:20:34 +0700

    Adds test

--- 6_3.c
+++ 6_3.c
@@ -19,16 +19,25 @@ float givenFunc(float x)
   return x*log(x);
 }
 
+void starting()
+{
+	for(float i = 1; i<=2.1; i+=0.1)
+	{
+		printf(" %.2f %.2f \n",i,givenFunc(i));
+	}
+}
+
 void interpolation()
 {
   for(int i = 0; i<=size; i++)
   {
-    printf("%d %.2f  \n",i+1, givenFunc(stepX(i)));
+    printf("%d %.2f %.2f \n",i+1,stepX(i), givenFunc(stepX(i)));
   }
 }
 
 int main()
 {
+	starting();
   interpolation();
 }
 
